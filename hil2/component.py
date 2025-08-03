0872552a06f4fb4ef76a515104a680b3b7b16304
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-08-03 15:54:35 -0600

    Do/DI/AO/AI function scafolding

---
+++ hil2/component.py
@@ -0,0 +1,62 @@
+from typing import Callable, Optional
+
+
+class DO:
+    """Digital Output"""
+
+    def __init__(self, set_fn: Callable[[bool], None], hiZ_fn: Callable[[], None]):
+        self._set_fn: Callable[[bool], None] = set_fn
+        self._hiZ_fn: Callable[[], None] = hiZ_fn
+
+    def set(self, value: bool) -> None:
+        self._set_fn(value)
+
+    def hiZ(self) -> None:
+        self._hiZ_fn()
+
+    def shutdown(self) -> None:
+        self._hiZ_fn()
+    
+
+class DI:
+    """Digital Input"""
+
+    def __init__(self, get_fn: Callable[[], bool]):
+        self._get_fn: Callable[[], bool] = get_fn
+
+    def get(self) -> bool:
+        return self._get_fn()
+    
+    def shutdown(self) -> None:
+        pass
+
+
+class AO:
+    """Analog Output"""
+
+    def __init__(self, set_fn: Callable[[float], None], hiZ_fn: Callable[[], None]):
+        self._set_fn: Callable[[float], None] = set_fn
+        self._hiZ_fn: Callable[[], None] = hiZ_fn
+
+    def set(self, value: float) -> None:
+        self._set_fn(value)
+
+    def hiZ(self) -> None:
+        self._hiZ_fn()
+
+    def shutdown(self) -> None:
+        self._hiZ_fn()
+
+
+class AI:
+    """Analog Input"""
+
+    def __init__(self, get_fn: Callable[[], float]):
+        self._get_fn: Callable[[], float] = get_fn
+
+    def get(self) -> float:
+        return self._get_fn()
+    
+    def shutdown(self) -> None:
+        pass
+
