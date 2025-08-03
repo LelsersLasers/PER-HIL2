b8529f1286e17546920e89963fd4e9832a31294d
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-08-03 16:55:41 -0600

    Using device manager

--- hil2/hil2.py
+++ hil2/hil2.py
@@ -1,6 +1,7 @@
 import os
 import cantools.database.can.database
 
+import test_device
 import component
 import net_map
 
@@ -14,18 +15,23 @@ class Hil2:
 		net_map_path: str,
 		can_dbc_path: str
 	):
-		...
 		self.net_map: net_map.NetMap = net_map.NetMap.from_csv(net_map_path)
+		self.test_device_manager: test_device.TestDeviceManager = test_device.TestDeviceManager.from_json(
+			test_config_path,
+			device_config_path
+		)
 		self.can_dbc: cantools.database.can.database.Database = cantools.db.load_file(os.path.join(can_dbc_path))
-
-	def set_ao(self, board: str, net: str, value: float) -> component.AO:
 		...
+
+	def set_ao(self, board: str, net: str, value: float) -> None:
+		net_map_entry = self.net_map.get_entry(board, net)
+		net_map_entry.component
+		net_map_entry.designator
 
 	def hiZ_ao(self, board: str, net: str) -> None:
 		...
 
 	def ao(self, board: str, net: str) -> component.AO:
-		...
 		return component.AO(
 			set_fn=lambda value: self.set_ao(board, net, value),
 			hiZ_fn=lambda: self.hiZ_ao(board, net)
