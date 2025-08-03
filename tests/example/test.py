203a67c94860a5c4e0c615b03ca38981d9573794
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-08-03 15:53:41 -0600

    Thinking about the script interface

---
+++ tests/example/test.py
@@ -0,0 +1,29 @@
+import hil2
+
+
+def hil2_init():
+	h = hil2.Hil2("config.json", "device_config.json", "netmap.csv", "can.dbc")
+	return h
+
+
+def main(h):
+	v_bat = h.ao("Main_Module", "VBatt")
+	v_bat.set(3.2)
+	val = v_bat.get()
+
+	h.set_ao("Main_Module", "VBatt", 3.2)
+	val = h.get_ao("Main_Module", "VBatt")
+	
+
+	mcan = h.can("HIL2", "MCAN")
+	mcan.send("Signal", {})
+	mcan.send(23, {})
+	sig_dict = mcan.get("Signal")
+	sig_dict = mcan.get(23)
+	mcan.clear("Signal")
+	mcan.clear(23)
+	mcan.clear()
+
+if __name__ == "__main__":
+	h = hil2_init()
+	main(h)
