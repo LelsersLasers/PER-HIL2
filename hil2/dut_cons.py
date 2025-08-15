17763cc35d6cf5a6a76582fa220fd010b5927e2b
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2025-08-15 10:46:57 -0700

    comments + fmt

--- hil2/dut_cons.py
+++ hil2/dut_cons.py
@@ -4,14 +4,26 @@ import hil_errors
 
 
 class HilDutCon:
+    """The HIL side of a DUT connection"""
+
     def __init__(self, device: str, port: str):
+        """
+        :param device: The name of the HIL device (ex: 'RearTester')
+        :param port: The port name on the HIL device (ex: 'DO7')
+        """
         self.device: str = device
         self.port: str = port
 
     @classmethod
-    def from_json(cls, hil_dut_con: dict) -> 'HilDutCon':
+    def from_json(cls, hil_dut_con: dict) -> "HilDutCon":
+        """
+        Create a HilDutCon instance from a JSON dictionary.
+
+        :param hil_dut_con: The JSON dictionary representing the HIL DUT connection
+        :return: A HilDutCon instance
+        """
         match hil_dut_con:
-            case { "device": device, "port": port }:
+            case {"device": device, "port": port}:
                 return cls(device, port)
             case _:
                 error_msg = f"Invalid HIL DUT connection configuration: {hil_dut_con}"
@@ -19,14 +31,26 @@ class HilDutCon:
 
 
 class DutCon:
+    """The tested side of a DUT connection"""
+
     def __init__(self, connector: str, pin: int):
+        """
+        :param connector: The name of the DUT connector (ex: 'J3')
+        :param pin: The pin number on the DUT connector (ex: 9)
+        """
         self.connector: str = connector
         self.pin: int = pin
 
     @classmethod
-    def from_json(cls, dut_con: dict) -> 'DutCon':
+    def from_json(cls, dut_con: dict) -> "DutCon":
+        """
+        Create a DutCon instance from a JSON dictionary.
+
+        :param dut_con: The JSON dictionary representing the DUT connection
+        :return: A DutCon instance
+        """
         match dut_con:
-            case { "connector": connector, "pin": pin }:
+            case {"connector": connector, "pin": pin}:
                 return cls(connector, pin)
             case _:
                 error_msg = f"Invalid DUT connection configuration: {dut_con}"
@@ -35,17 +59,26 @@ class DutCon:
 
 class DutBoardCons:
     def __init__(self, harness_connections: dict[DutCon, HilDutCon]):
+        """
+        :param harness_connections: A dictionary mapping DUT connections to HIL connections
+        """
         self._harness_connections = harness_connections
 
     @classmethod
-    def from_json(cls, harness_connections: list[dict]) -> 'DutBoardCons':
+    def from_json(cls, harness_connections: list[dict]) -> "DutBoardCons":
+        """
+        Create a DutBoardCons instance from a JSON dictionary.
+
+        :param harness_connections: A list of dictionaries representing the harness connections
+        :return: A DutBoardCons instance
+        """
         parsed_connections: dict[DutCon, HilDutCon] = {}
 
         for con in harness_connections:
             match con:
-                case { "dut": dut_con, "hil": hil_con }:
-                    parsed_connections[DutCon.from_json(dut_con)] = (
-                        HilDutCon.from_json(hil_con)
+                case {"dut": dut_con, "hil": hil_con}:
+                    parsed_connections[DutCon.from_json(dut_con)] = HilDutCon.from_json(
+                        hil_con
                     )
                 case _:
                     error_msg = f"Invalid DUT board connection configuration: {con}"
@@ -54,6 +87,12 @@ class DutBoardCons:
         return cls(parsed_connections)
 
     def get_hil_device_connection(self, dut_con: DutCon) -> HilDutCon:
+        """
+        Get the HIL device connection for a given DUT connection.
+
+        :param dut_con: The DUT connection for which to retrieve the HIL device connection
+        :return: The corresponding HIL device connection
+        """
         if dut_con in self._harness_connections:
             return self._harness_connections[dut_con]
         else:
@@ -65,23 +104,32 @@ class DutBoardCons:
 
 
 class DutCons:
-    def __init__(self, dut_connections: dict[DutBoardCons]):
+    def __init__(self, dut_connections: dict[str, DutBoardCons]):
+        """
+        :param dut_connections: A dictionary mapping DUT board names to their connections
+        """
         self._dut_connections: dict[str, DutBoardCons] = dut_connections
 
     @classmethod
-    def from_json(cls, test_config_path: str) -> 'DutCons':
-        with open(test_config_path, 'r') as test_config_file:
+    def from_json(cls, test_config_path: str) -> "DutCons":
+        """
+        Create a DutCons instance from a JSON configuration file.
+
+        :param test_config_path: The path to the JSON configuration file
+        :return: A DutCons instance
+        """
+        with open(test_config_path, "r") as test_config_file:
             test_config = json.load(test_config_file)
 
         board_cons = {}
         match test_config:
-            case { "dut_connections": dut_connections }:
+            case {"dut_connections": dut_connections}:
                 for board_cons in dut_connections:
                     ...
                     match board_cons:
                         case {
                             "board": board,
-                            "harness_connections": harness_connections
+                            "harness_connections": harness_connections,
                         }:
                             board_cons[board] = DutBoardCons.from_json(
                                 harness_connections
@@ -93,11 +141,19 @@ class DutCons:
                             )
                             raise hil_errors.ConfigurationError(error_msg)
             case _:
-                pass # Not an error to have no DUT connections
+                # Not an error to have no DUT connections
+                pass
 
         return cls(board_cons)
-    
+
     def get_hil_device_connection(self, board: str, dut_con: DutCon) -> HilDutCon:
+        """
+        Get the HIL device connection for a given DUT connection.
+
+        :param board: The name of the DUT board
+        :param dut_con: The DUT connection for which to retrieve the HIL device connection
+        :return: The corresponding HIL device connection
+        """
         if board in self._dut_connections:
             return self._dut_connections[board].get_hil_device_connection(dut_con)
         else:
