1f040c2c378ed821928d1ef8cdb6c539d688d324
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2025-08-17 19:39:31 -0700

    Updating scafolding?

--- tests/example/test.py
+++ tests/example/test.py
@@ -1,34 +1,44 @@
-import hil2
+import hil2.hil2 as hil2
+import mk_assert.mk_assert as mka
 
 
-def hil2_init():
-    h = hil2.Hil2("config.json", "device_config.json", "netmap.csv", "can.dbc")
-    return h
+def do_test(h: hil2.Hil2):
+    do_2 = h.do("MainTester", "DO@2")
+    di_28 = h.di("MainTester", "DI@28")
+
+    do_2.set(True)
+    val = di_28.get()
+    mka.assert_true(val, "DI@28 should be True after setting DO@2 to True")
 
 
-def main(h: hil2.Hil2):
-    v_bat = h.ao("Main_Module", "VBatt")
-    v_bat.set(3.2)
-    val = v_bat.get()
 
-    h.set_ao("Main_Module", "VBatt", 3.2)
-    val = h.get_ao("Main_Module", "VBatt")
+def main():
+    h = hil2.Hil2("./tests/example/config.json", "device_configs", "netmap.csv", "can.dbc")
+    mka.add_test(do_test, h)
 
-    h.get_last_can("HIL2", "MCAN", "Signal")
+    mka.run_tests()
+
+    # v_bat = h.ao("Main_Module", "VBatt")
+    # v_bat.set(3.2)
+    # val = v_bat.get()
+
+    # h.set_ao("Main_Module", "VBatt", 3.2)
+    # val = h.get_ao("Main_Module", "VBatt")
+
+    # h.get_last_can("HIL2", "MCAN", "Signal")
     
 
-    mcan = h.can("HIL2", "MCAN")
-    mcan.send("Signal", {})
-    mcan.send(23, {})
-    sig_dict = mcan.get_last("Signal")
-    sig_dict = mcan.get_last(23)
-    sig_dicts = mcan.get_all("Signal")
-    sig_dicts = mcan.get_all(23)
-    sig_dicts = mcan.get_all()
-    mcan.clear("Signal")
-    mcan.clear(23)
-    mcan.clear()
+    # mcan = h.can("HIL2", "MCAN")
+    # mcan.send("Signal", {})
+    # mcan.send(23, {})
+    # sig_dict = mcan.get_last("Signal")
+    # sig_dict = mcan.get_last(23)
+    # sig_dicts = mcan.get_all("Signal")
+    # sig_dicts = mcan.get_all(23)
+    # sig_dicts = mcan.get_all()
+    # mcan.clear("Signal")
+    # mcan.clear(23)
+    # mcan.clear()
 
 if __name__ == "__main__":
-    h = hil2_init()
-    main(h)
+    main()
