902c636d66e2ef4f233f8179eacb89857a011e3c
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2025-08-17 19:30:25 -0700

    Figuring out prints for mk_assert

--- mk_assert/mk_assert.py
+++ mk_assert/mk_assert.py
@@ -2,6 +2,8 @@ from typing import Callable, Any, Optional
 
 import logging
 
+import print_helper
+
 
 # Global test state -------------------------------------------------------------------#
 g_tests: list["TestFn"] = []
@@ -12,20 +14,19 @@ class TestFn:
     def __init__(
         self, func: Callable[..., None], args: tuple[Any, ...], kwargs: dict[str, Any]
     ):
-        self.func = func
-        self.args = args
-        self.kwargs = kwargs
+        self.func: Callable[..., None] = func
+        self.args: tuple[Any, ...] = args
+        self.kwargs: dict[str, Any] = kwargs
 
-    def run(self) -> Any:
-        logging.info(f"Running test: {self.func.__name__}")
+    def run(self) -> None:
         return self.func(*self.args, **self.kwargs)
 
 
 class ActiveTestContext:
     def __init__(self, test_fn: TestFn):
         self._test_fn = test_fn
-        self._passed: int = 0
-        self._failed: int = 0
+        self.passed: int = 0
+        self.failed: int = 0
 
     def __enter__(self):
         global g_active_test
@@ -38,22 +39,17 @@ class ActiveTestContext:
 
         if exc_type is not None:
             self.failure()
-            logging.critical(f"Test '{self._test_fn.func.__name__}' raised an exception: {exc_value}")
-        
+            logging.critical(
+                f"Test '{self._test_fn.func.__name__}' raised an exception: {exc_value}"
+            )
+
         return False
 
     def success(self):
-        self._passed += 1
+        self.passed += 1
 
     def failure(self):
-        self._failed += 1
-
-    def summary(self) -> str:
-        total = self._passed + self._failed
-        return (
-            f"Test '{self._test_fn.func.__name__}':"
-            f"{self._passed}/{total} passed, {self._failed} failed."
-        )
+        self.failed += 1
 
 
 def add_test(func, *args, run_now: bool = False, **kwargs):
@@ -69,16 +65,24 @@ def add_test(func, *args, run_now: bool = False, **kwargs):
 
 def run_single_test(test_fn: TestFn) -> None:
     with ActiveTestContext(test_fn) as active_test:
+        logging.debug(f"Running test: {test_fn.func.__name__}")
+
+        print_helper.print_test_start(test_fn.func.__name__)
         test_fn.run()
-        print(active_test.summary())
+        print_helper.print_test_summary(
+            test_fn.func.__name__,
+            active_test.passed,
+            active_test.failed,
+        )
 
 
 def run_tests():
     global g_tests
-    
+
     logging.debug(f"Running {len(g_tests)} tests...")
     for test_fn in g_tests:
         run_single_test(test_fn)
+        print()
 
 
 def clear_tests():
@@ -87,24 +91,18 @@ def clear_tests():
     g_tests.clear()
 
 
-def assert_eq(cond: bool, msg: str = "", negate: bool = False):
+def assert_true(cond: bool, msg: str = "", negate: bool = False):
     global g_active_test
     if g_active_test is None:
         raise RuntimeError("No active test context for assertion.")
 
     if cond != negate:
-        if msg == "":
-            logging.debug("Check passed.")
-        else:
-            logging.debug(f"Check passed: {msg}")
         g_active_test.success()
     else:
-        if msg == "":
-            logging.warning("Check failed.")
-        else:
-            logging.warning(f"Check failed: {msg}")
         g_active_test.failure()
+
+    print_helper.print_assert(msg, cond != negate)
 
 
 def assert_eqf(a: float, b: float, tol: float, msg: str = "", negate: bool = False):
-    assert_eq(abs(a - b) <= tol, msg, negate)
+    assert_true(abs(a - b) <= tol, msg, negate)
