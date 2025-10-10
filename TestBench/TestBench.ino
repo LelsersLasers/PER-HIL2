7e76ca09b3d319f2fbbfa54f5a2c10b5bd3a73c6
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-10-09 21:34:37 -0400

    It works on my computer???

--- TestBench/TestBench.ino
+++ TestBench/TestBench.ino
@@ -158,7 +158,7 @@ void loop() {
             pinMode(pin, INPUT);
             int val = digitalRead(pin);
             SERIAL_CON.write(SerialCommand::READ_GPIO);
-            SERIAL_CON.write(1);
+            SERIAL_CON.write(val);
             break;
         }
         case SerialCommand::WRITE_DAC: {
